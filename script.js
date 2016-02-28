603985cdd3daccba5c131c0517b3dc256865164c
wass80 <wasss80@gmail.com>
2016-02-28 12:06:08 +0000

    get atcoder object

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+function main(){
+  console.log(ATCODER);
+}
+
+const script = document.createElement("script");
+script.appendChild(document.createTextNode(`(${main.toString()})()`));
+document.body.appendChild(script);
